2ec8db98356b1cf650c51f2c8d59f6cbf5d81ec5
Mouayad <muayad.perun@outlook.com>
2025-01-22 21:59:21 +0200

    refactor: add seo to `siteData`

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,4 +1,9 @@
 export const siteData = {
+    seo: {
+        myName: "Mouayad Alhamwi",
+        homeTitle: "Portfolio",
+        description: "Welcome to my corner of web. My name is Mouayad Alhamwi, I'm a Muslim Software Engineer focused on Backend Development and I do some front-end dev on the side with Astro & TailwindCSS",
+    },
     hero: {
         name: "Mouayad Alhamwi",
         nameArabic: "مؤَيّد الحموي مَمشوق",
