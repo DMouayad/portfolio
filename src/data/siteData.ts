c2c3b02127918604c8b4e19d7e13a1cc7cd49907
Mouayad <muayad.perun@outlook.com>
2025-01-23 09:06:42 +0200

    feat: new hero section
    - refactor: replace `MY_CONTACTS` constant with `siteData.contactInfo`.
    - feat: improve `ProjectCard` design on mobile.

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -7,5 +7,33 @@ export const siteData = {
     hero: {
         name: "Mouayad Alhamwi",
         nameArabic: "مؤَيّد الحموي مَمشوق",
+        aboutMeTitle: "I'm a freelance software engineer interested in back-end development.",
+        aboutMeText: "I love to experiment with anything related to tech & software. Currently, I'm focused on developing fast, secure and visually appealing apps. I'm also interested in Web Dev, User Experience, Clean Code, the Universe and its Creator."
+    },
+    contactInfo: {
+        email: {
+            title: 'Email',
+            icon: 'carbon:email',
+            url: 'mailto:mouayad.alhamwi.ma@gmail.com',
+            buttonLabel: "Contact me via Email"
+        },
+        github: {
+            title: 'Github',
+            icon: 'carbon:logo-github',
+            url: 'https://github.com/DMouayad',
+            buttonLabel: "View my GitHub account"
+        },
+        // telegram: {
+        //     title: 'Telegram',
+        //     icon: 'mdi:telegram',
+        //     url: 'https://t.me/DDragon13',
+        //     buttonLabel: "Contact me via Telegram"
+        // },
+        linkedIn: {
+            title: 'LinkedIn',
+            icon: 'carbon:logo-linkedin',
+            url: 'https://www.linkedin.com/in/mouayad-alhamwi-85bb9420b/',
+            buttonLabel: "View my LinkedIn account"
+        }
     }
-} 
+}
